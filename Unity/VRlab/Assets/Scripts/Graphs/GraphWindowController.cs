f1c04e7c7cad4384199abb67bbc3f3b1b4dc23a2
Rafa <rpadiper@gmail.com>
2022-06-28 18:39:22 -0700

    Working on graphs

--- Unity/VRlab/Assets/Scripts/Graphs/GraphWindowController.cs
+++ Unity/VRlab/Assets/Scripts/Graphs/GraphWindowController.cs
@@ -16,13 +16,29 @@ public class GraphWindowController : MonoBehaviour
 
     private RectTransform graphContainer;
 
-    float[] ceramicXValues, ceramicYValues;
+    private float[] ceramicXValues, ceramicYValues; // Ceramic
+    private float[] metalXValues, metalYValues; // Metal
+    private float[] polymerXValues, polymerYValues; // Polymer
+
+    private float xMax, yMax;
+
 
     private void Awake()
     {
         graphContainer = transform.Find("graphContainer").GetComponent<RectTransform>();
+        // Ceramic data
         ceramicXValues = new float[] { 0, 10, 20, 30, 40, 50, 60, 70, 100 };
         ceramicYValues = new float[] { 0, 10, 20, 30, 40, 50, 60, 70, 100 };
+
+        // Metal data
+        metalXValues = new float[] { };
+        metalXValues = new float[] { };
+
+        // Polymer data
+        polymerXValues = new float[] { };
+        polymerXValues = new float[] { };
+
+        // --TEST ONLY--TEST ONLY--TEST ONLY--
         RenderGraph(ceramicXValues, ceramicYValues);
     }
     // Creates a pint in graphContainer given a x,y vector2 position < size.x, size.y
@@ -49,10 +65,12 @@ public class GraphWindowController : MonoBehaviour
         float graphHeight = graphContainer.sizeDelta.y;
         float graphWidth = graphContainer.sizeDelta.x;
 
-        float xMax = Mathf.Max(xValues); // Maximum possible X value (from data)
-        float yMax = Mathf.Max(yValues); // Maximum possible Y value (from data)
+        xMax = Mathf.Max(xValues); // Maximum possible X value (from data)
+        yMax = Mathf.Max(yValues); // Maximum possible Y value (from data)
 
         GameObject previousPointObject = null;
+
+        // Will need to find a way for this for loop to run slow so we can see the plot going
         for (int i = 0; i < xValues.Length; i++)
         {
             // Normalize to local graph size
@@ -92,5 +110,14 @@ public class GraphWindowController : MonoBehaviour
 
         // Rotate at angle between the 2 points
         rectTransform.localEulerAngles = new Vector3(0, 0, Mathf.Rad2Deg * Mathf.Atan(dir.y/dir.x));
+    }
+
+    private void RenderXaxis()
+    {
+
+    }
+    private void RenderYaxis()
+    {
+
     }
 }
