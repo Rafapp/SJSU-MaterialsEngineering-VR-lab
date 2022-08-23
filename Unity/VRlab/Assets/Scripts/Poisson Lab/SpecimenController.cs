0452a128bca520577f9cd8850f52aaa36df02d28
Rafapp <rpadiper@gmail.com>
2022-08-23 11:24:52 -0700

    Cylinder working

--- Unity/VRlab/Assets/Scripts/Poisson Lab/SpecimenController.cs
+++ Unity/VRlab/Assets/Scripts/Poisson Lab/SpecimenController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SpecimenController : MonoBehaviour
 {
+    private enum ObjectType{ Cylinder, Cube }
+
     [SerializeField]
     private Transform handle1, handle2;
 
@@ -11,35 +13,63 @@ public class SpecimenController : MonoBehaviour
     private GameObject specimen, transparentSpecimen;
 
     [SerializeField]
-    private float poissonRatio, offset;
+    private float poissonRatio, cubeOffset, cylinderOffset;
 
-    private Vector3 initialScale;
+    private Vector3 transparentOffset;
+
+    [SerializeField]
+    private ObjectType obj;
 
     private float initialDistance;
 
     private void Awake()
     {
+        transparentOffset = new Vector3(.01f, .01f, .01f);
+
         initialDistance = (handle1.position - handle2.position).magnitude;
-        initialScale = gameObject.GetComponent<Transform>().localScale;
 
-        // Elongate or compress the shape using the handles, center shape
-        specimen.transform.localScale = new Vector3(specimen.transform.localScale.z * poissonRatio + offset, specimen.transform.localScale.z * poissonRatio + offset,
-            ((handle1.position - handle2.position).magnitude - (handle1.transform.localScale.z)));
+        if (obj == ObjectType.Cube)
+        {
+            // Elongate or compress the shape using the handles, center shape
+            specimen.transform.localScale = new Vector3(specimen.transform.localScale.z * poissonRatio + cubeOffset, specimen.transform.localScale.z * poissonRatio + cubeOffset,
+                ((handle1.position - handle2.position).magnitude - (handle1.transform.localScale.z)));
 
-        // Update the transparent shape to match
-        transparentSpecimen.transform.localScale = specimen.transform.localScale;
+            // Update the transparent shape to match
+            transparentSpecimen.transform.localScale = specimen.transform.localScale + transparentOffset;
+        }
+        if (obj == ObjectType.Cylinder) {
+            // Elongate or compress the shape using the handles, center shape
+            specimen.transform.localScale = new Vector3(specimen.transform.localScale.y * poissonRatio + cylinderOffset,
+                ((handle1.position - handle2.position).magnitude - (handle1.transform.localScale.z)) / 2, specimen.transform.localScale.y * poissonRatio + cylinderOffset);
+
+            // Update the transparent shape to match
+            transparentSpecimen.transform.localScale = specimen.transform.localScale + transparentOffset;
+        }
     }
     private void Update()
     {
         //Note: This is intensive, must only happen when grabbing both handles
 
-        // Elongate or compress the shape using the handles, center shape
-        specimen.transform.localScale = new Vector3(specimen.transform.localScale.z * poissonRatio + offset, specimen.transform.localScale.z * poissonRatio + offset,
-            ((handle1.position - handle2.position).magnitude - (handle1.transform.localScale.z)));
+        if (obj == ObjectType.Cube)
+        {
+            // Elongate or compress the shape using the handles, center shape
+            specimen.transform.localScale = new Vector3(specimen.transform.localScale.z * poissonRatio + cubeOffset, specimen.transform.localScale.z * poissonRatio + cubeOffset,
+                ((handle1.position - handle2.position).magnitude - (handle1.transform.localScale.z)));
 
-        // Center the cube between the handles
-        specimen.transform.localPosition = (handle1.localPosition + handle2.localPosition)*0.5f;
-        transparentSpecimen.transform.localPosition = (handle1.localPosition + handle2.localPosition) * 0.5f;
+            // Center the cube between the handles
+            specimen.transform.localPosition = (handle1.localPosition + handle2.localPosition) * 0.5f;
+            transparentSpecimen.transform.localPosition = (handle1.localPosition + handle2.localPosition) * 0.5f;
+        }
+        else if (obj == ObjectType.Cylinder) {
+            // Elongate or compress the shape using the handles, center shape
+            specimen.transform.localScale = new Vector3(specimen.transform.localScale.y * poissonRatio + cylinderOffset,
+                ((handle1.position - handle2.position).magnitude - (handle1.transform.localScale.z))/2, specimen.transform.localScale.y * poissonRatio + cylinderOffset);
+
+            // Center the cylinder between the handles
+            specimen.transform.localPosition = (handle1.localPosition + handle2.localPosition) * 0.5f;
+            transparentSpecimen.transform.localPosition = (handle1.localPosition + handle2.localPosition) * 0.5f;
+        }
+
     }
     public float getValue()
     {
